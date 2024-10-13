bdfb75f48d0cde38c4413af9fdd7466e6ae2bb44
fabiofavaleiro <fabiofavaleiro@gmail.com>
2024-10-13 20:45:15 -0300

    000

---
+++ src/controller/ProductController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import entity.Product;
+import repository.ProductRepositoryImpl;
+import service.ProductService;
+
+import java.util.Set;
+
+public class ProductController {
+    ProductService productService = new ProductService(new ProductRepositoryImpl());
+    public ProductController() {
+
+    }
+
+    public void deleteProduct(int id) {
+        productService.delete(id);
+    }
+
+    public void updateProduct(int id, Product product) {
+        productService.update(id, product);
+    }
+
+    public void addProduct(Product product) {
+        productService.save(product);
+    }
+
+    public Set<Product> getProducts() {
+        return productService.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return productService.findById(id);
+    }
+}
---
+++ src/service/ProductService.java
@@ -0,0 +1,35 @@
+package service;
+
+import entity.Product;
+import repository.ProductRepository;
+
+import java.util.Set;
+
+public class ProductService {
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    public void delete(int id) {
+        productRepository.delete(id);
+    }
+
+    public void update(int id, Product product) {
+        productRepository.update(id, product);
+    }
+
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+
+    public Set<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(int id) {
+        return productRepository.findById(id);
+    }
+}
+
